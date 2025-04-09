98a1d0125f9a4c8f45fa0d2dfb6a00d3e64fd524
Lucas Almeida Bel Correa <lucasbel.correa@gmail.com>
2025-04-09 09:39:27 -0300

    Atualização do projeto de fornecedor

--- src/br/fiap/util/Util.java
+++ src/br/fiap/util/Util.java
@@ -13,6 +13,7 @@ public class Util {
     private Produto[] produtos = new Produto[N];
     private int p = 0;
     private int f = 0;
+    private long cnpjFornecedor = 0;
 
     public void menu() {
         String menuText = "1.Cadastrar Produto\n2.Pesquisar Produto por Nome" +
@@ -27,55 +28,94 @@ public class Util {
 
             switch (op) {
                 case 1:
-                    cadastrarProduto();
+                    cnpjFornecedor = parseLong(showInputDialog("Insira o valor do CNPJ do fornecedor"));
+                    cadastrarProduto(cnpjFornecedor);
                     break;
                 case 2:
-                    //pesquisarProduto();
+                    showMessageDialog(null, exibirInfoProduto());
                     break;
                 case 3:
-                    //pesquisarFornecedor();
+                    pesquisarFornecedor();
                     break;
             }
         } while (op != 4);
     }
 
-    private void cadastrarProduto() {
+    private void cadastrarProduto(long cnpjFornecedor) {
         String nomeProduto;
         double valorUnitario;
         int qtdEstoque;
-        long cnpjFornecedor;
+        Fornecedor aux = localizarFornecedor(cnpjFornecedor);
 
-        cnpjFornecedor = parseLong(showInputDialog("Insira o valor do CNPJ do fornecedor"));
-
-        if (localizarFornecedor(cnpjFornecedor) != null) {
-            nomeProduto = showInputDialog("Insira o nome do produto");
-            valorUnitario = parseDouble(showInputDialog("Insira o valor unitário do produto"));
-            qtdEstoque = parseInt(showInputDialog("Insira a quantidade em estoque do produto"));
-
-            produtos[p] = new Produto(nomeProduto, valorUnitario, qtdEstoque, fornecedores[f]);
-        } else {
-
+        if(aux == null) {
+            aux = cadastrarFornecedor();
         }
+
+        nomeProduto = showInputDialog("Insira o nome do produto");
+        valorUnitario = parseDouble(showInputDialog("Insira o valor unitário do produto"));
+        qtdEstoque = parseInt(showInputDialog("Insira a quantidade em estoque do produto"));
+        produtos[p] = new Produto(nomeProduto, valorUnitario, qtdEstoque, aux);
+        p++;
     }
 
-    private void cadastrarFornecedor() {
+    private Fornecedor cadastrarFornecedor() {
         String nome;
         long cnpj;
 
         nome = showInputDialog("Insira o nome do fornecedor");
-        cnpj = parseLong("Insira o CNPJ do fornecedor");
+        cnpj = parseLong(showInputDialog("Insira o CNPJ do fornecedor"));
 
         fornecedores[f] = new Fornecedor(nome, cnpj);
         f++;
+
+        return fornecedores[f-1];
     }
 
-    private Fornecedor localizarFornecedor(long cnpj) {
+    private Fornecedor localizarFornecedor(long cnpjFornecedor) {
         for(int j = 0; j < f; j++) {
-            if (fornecedores[j].getCnpj() == cnpj) {
+            if (fornecedores[j].getCnpj() == cnpjFornecedor) {
                 return fornecedores[j];
             }
         }
-        showMessageDialog(null, "CNPJ " + cnpj + " não cadastrado!");
+        showMessageDialog(null, "CNPJ " + cnpjFornecedor + " não cadastrado!");
         return null;
+    }
+
+    private Produto pesquisarProduto() {
+        String nomeProduto = showInputDialog("Insira o nome do produto que deseja buscar");
+        for(int i = 0; i < p; i++) {
+            if (nomeProduto.equalsIgnoreCase(produtos[i].getNome())) {
+                return produtos[i];
+            }
+        }
+        showMessageDialog(null, "Produto não encontrado!");
+        return null;
+    }
+
+    private String exibirInfoProduto() {
+        Produto produto = pesquisarProduto();
+        String aux = "Produto não encontrado";
+
+        if(produto != null) {
+            aux = "";
+            aux += "Produto: " + produto.getNome();
+            aux += "\nQuantidade em estoque: " + produto.getQtdEstoque();
+            aux += "\nValor unitário: R$" + String.format("%.2f", produto.getValorUnitario());
+            aux += "\nFornecedor: " + produto.getFornecedor().getNome();
+            return aux;
+        }
+        return aux;
+    }
+
+    private void pesquisarFornecedor() {
+        Fornecedor fornecedor = localizarFornecedor(cnpjFornecedor);
+        String aux = "";
+        if (fornecedor != null) {
+            aux += "\nFornecedor: " + fornecedor.getNome();
+            aux += "\nCNPJ: " + fornecedor.getCnpj();
+            showMessageDialog(null, aux);
+        } else {
+            showMessageDialog(null, "Fornecedor não encontrado!");
+        }
     }
 }
